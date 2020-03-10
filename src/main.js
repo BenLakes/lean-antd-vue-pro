6e92ed27c13358bb356a27ed2a31c7ee493bce6b
linanzhi <604429058@qq.com>
2020-03-10 15:57:25 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import 'ant-design-vue/dist/antd.less'
+// 没在babel 按需 配置的时候的写法
+// import Button from "ant-design-vue/lib/button";
+// import 'ant-design-vue/lib/button/style';
 
+import {Button} from "ant-design-vue";
+
+// Vue.use(Antd);
+Vue.use(Button);
 Vue.config.productionTip = false;
 
 new Vue({
